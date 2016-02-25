35d70e318552c941957deb334bd769b012dbdacb
Haroon Ghawsi <ghawsiharoon@gmail.com>
2016-02-25 01:08:22 +0100

    Registration form added

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 $(document).ready(function(){
 	setTimeout(popup, 3000);
 	function popup(){
-		$("#logindiv").css("display", "block");
+		$("#ask").css("display", "block");
 	}
-	$("#login #cancel").click(function(){
+	$("#sgnN").click(function(){
+		$("#logindiv").css("display", "block");
+	});
+	$("#sgnUP").click(function(){
+		$("#register").css("display", "block");
+	});
+	$("#rcancel").click(function(){
+		$(this).parent().parent().hide();
+	});
+	$("#guest").click(function(){
+		$(this).parent().parent().hide();
+	});
+	$("#cancel").click(function(){
 		$(this).parent().parent().hide();
 	});
 });
-$("#loginbtn").click(function(){
-	var name = $("#username").val();
-	var password = $("#password").val();
-	if(username == "" || password == ""){
-		alert("Username or Password was wrong");
-	}else{
-		$("#logindiv").css("display", "none");
-	}
-});
+	
