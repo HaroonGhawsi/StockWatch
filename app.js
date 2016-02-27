fba5929333333793549cf2c70b08a46cddbd8b1a
Haroon Ghawsi <ghawsiharoon@gmail.com>
2016-02-27 03:53:21 +0100

    Form Layout changed

--- app.js
+++ app.js
@@ -1,28 +1,22 @@
+
 $(document).ready(function(){
-	setTimeout(popup, 3000);
-	function popup(){
-		$("#ask").fadeIn();
-	}
 	$("#sgnN").click(function(){
 		$("#logindiv").fadeIn();
 	});
-	$("#sgnUP").click(function(){
+	$(".sgnUP").click(function(){
 		$("#register").fadeIn();
 	});
-	$("#rcancel").click(function(){
+	$(".rcancel").click(function(){
 		$(this).parent().parent().fadeOut();
 	});
-	$("#close").click(function(){
-		$(this).parent().fadeOut();
-	});
-	$("#cancel").click(function(){
+	$(".cancel").click(function(){
 		$(this).parent().parent().fadeOut();
 	});
 	$("#results").hide();
 	
 });	
 $(document).ready(function(){
-	$("#submit").click(function(){	
+	$(".submit").click(function(){	
 		
 		var fname = $("#fname").val();
 		var lname = $("#lname").val();
