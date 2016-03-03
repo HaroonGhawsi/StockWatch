c2e29b8552e886c04f861e08031a4b9b54040066
Haroon Ghawsi <ghawsiharoon@gmail.com>
2016-03-03 01:32:27 +0100

    v1.0 Layout changed

--- app.js
+++ app.js
@@ -1,65 +1,18 @@
-
-$(document).ready(function(){
-	$("#sgnN").click(function(){
-		$("#logindiv").fadeIn();
-	});
-	$(".sgnUP").click(function(){
-		$("#register").fadeIn();
-	});
-	$(".rcancel").click(function(){
-		$(this).parent().parent().fadeOut();
-	});
-	$(".cancel").click(function(){
-		$(this).parent().parent().fadeOut();
-	});
-	$("#results").hide();
-	
-});	
-$(document).ready(function(){
-	$(".submit1").click(function(){	
-		
-		var fname = $("#fname").val();
-		var lname = $("#lname").val();
-		var pwd = $("#pwd").val();
-		var rpwd = $("#rpwd").val();
-		var eml = $("#eml").val();
-		
-				
-		if(fname == "" || lname == "" || pwd == "" || rpwd == "" || eml == ""){
-			
-			$("#registration").hide();
-			$("#ask1").hide();
-			$("#results").fadeIn();
-			$("#errmsg").append("Empty Error!");
-			
-			$("#ok").click(function(){
-				$("#results").fadeOut();
-				$("#registration").fadeIn();
-			});
-			
-		}
-		else{
-			$("#registration").hide();
-			$("#ask1").hide();
-			$("#results").fadeIn();
-			$("#errmsg").append("Success!");
-			$("#ok").click(function(){
-				$("#results").hide();
-			});
-		}
-	});
-		$("#registration").submit(function (event){
-		alert("Handler for .submit() called.");
-		event.preventDefault();
-	});
-	
-	
+$(function(){
+    var pull = $('#pull');
+    var menu = $('nav ul');
+    menuheight = menu.height();
+    
+    $(pull).on("click", function(e){
+        e.preventDefault();
+        menu.slideToggle();
+    });
 });
 
+$(window).resize(function(){
+    var w = $(window).width();
+    if(w > 320 && menu.is(':hidden')){
+        menu.removeAttr('style');
+    }
+});
 
-	
-	
-	
-	
-	
-	
